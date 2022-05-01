eb0072df773f2305fe687a23c9d1fcb557d7a28e
Jihong Min <jihong729@naver.com>
2022-05-01 11:57:49 +0900

    Initial Commit contains practice code for SQL Basics

---
+++ practice.sql
@@ -0,0 +1,35 @@
+// this is practice code that was made from the sqlitonline.com
+
+//CREATING TABLES and INSERTING DATA
+
+CREATE TABLE customers (
+	id INT NOT NULL,
+  	first_name STRING,
+  	last_name STRING,
+  	address STRING,
+  	PRIMARY KEY (id)
+)
+
+CREATE TABLE products (
+	id INT NOT NULL,
+  	name STRING,
+  	price MONEY, stock INT,
+  	PRIMARY KEY (id)
+)
+
+CREATE TABLE orders (
+	id INT NOT NULL,
+  	order_number INT,
+  	customer_id INT,
+  	product_id INT,
+  	PRIMARY KEY (id),
+  	FOREIGN KEY (customer_id) REFERENCES customers(id),
+  	FOREIGN KEY (product_id) REFERENCES products(id)
+)
+
+
+//How to use Foreign Keys and Inner Joins
+
+SELECT orders.order_number, customers.first_name, customers.last_name, customers.address
+FROM orders
+INNER JOIN customers ON orders.customer_id = customers.id
